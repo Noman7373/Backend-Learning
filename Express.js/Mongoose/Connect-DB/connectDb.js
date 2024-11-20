e822909aea753b0e615e8881290361e1afe5a555
NomanAhmed <officialnoman515@gmail.com>
2024-11-20 14:09:50 +0300

    Mongoose

---
+++ Express.js/Mongoose/Connect-DB/connectDb.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const connectDB = async (URL) => {
+  try {
+    // await mongoose.connect(URL);
+    //  with auth
+    const DB_OPTIONS = {
+      user: "Noman Ahmed",
+      pass: "12345678",    // if the password have special character 
+      dbName: "students",
+      authSource: "students", // database name
+    };
+
+    await mongoose.connect(URL, DB_OPTIONS);
+    console.log("Connected Successfully");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default connectDB;
