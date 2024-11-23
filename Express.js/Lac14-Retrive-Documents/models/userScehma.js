73382e4e828b2c0c4a2039fd0910d3bd6e83aecc
NomanAhmed <officialnoman515@gmail.com>
2024-11-23 16:11:01 +0300

    practice Comparison operator

--- Express.js/Lac14-Retrive-Documents/models/userScehma.js
+++ Express.js/Lac14-Retrive-Documents/models/userScehma.js
@@ -70,15 +70,30 @@ const getAllData = async () => {
   //   const userNameAge4 = await userModel.find().select("-name -age"); // method 1 exclude
   //   const userNameAge5 = await userModel.find().select([ '-name', '-age' ]); // method 2 exclude
   //   const userNameAge6 = await userModel.find().select({ name: 0, age: 0 }); //  method 2 exclude
-
   //   ================   getData with limit() , skip() , count() ,sorting()  =========================
-
   //   const getLimitData = await userModel.find().limit(1);
   //   const getLimitData1 = await userModel.find().limit(1);
   //   const getLimitData2 = await userModel.find().skip(1);
   //   const getLimitData3 = await userModel.find().countDocuments();
   //   const getLimitData4 = await userModel.find().sort({ age: 1 });
-  const getLimitData5 = await userModel.find().sort({ age: -1 });
-  console.log(getLimitData5);
+  //   const getLimitData5 = await userModel.find().sort({ age: -1 });
+  //   console.log(getLimitData5);
+  //   ================   Comparison Operator  ($eq , $ne, $gt , $gte , $lt, $lte , $in , $nin) =========================
+
+  //   const data1 = await userModel.find({ age: { $eq: 24 } });
+  //   const data2 = await userModel.find({ age: { $nt: 24 } });
+  //   const data3 = await userModel.find({ age: { $gt: 24 } });
+  //   const data4 = await userModel.find({ age: { $gte: 24 } });
+  //   const data5 = await userModel.find({ age: { $lt: 24 } });
+  //   const data6 = await userModel.find({ age: { $lte: 24 } });
+  //   const data7 = await userModel.find({ age: { $in: [22, 25] } });
+  //   const data8 = await userModel.find({ age: { $nin: [22, 25] } }); // not in
+  //   console.log(data8);
+
+  //   ===================      Logical Operator  ($and()  ,  $or() )   ==================================
+  //   const logical1 = await userModel.find({$and: [{ name: "Nomi Khan" }, { age: 25 }]});   // $and both field should exits
+//   const logical2 = await userModel.find({$or: [{ name: "Nomi Khan" }, { age: 25 }]}); // $or at least one field should exits
+  const logical3 = await userModel.find({$or: [{ name: "Nomi Khan" }, { age: 25 }]}); // $or at least one field should exits
+  console.log(logical3);
 };
 export { getAllData };
